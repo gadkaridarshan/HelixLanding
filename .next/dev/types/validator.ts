22f5ef7cf5a73bb018e9bc55856c13f6d82e54e9
Darshan Gadkari <darshangadkari14@gmail.com>
2026-09-17 07:49:49 -0500

    [Helix USER-421000] Fix Footer CSS
    Card: Fix Footer CSS (development)
    Story ID: USER-421000
    
    Committed automatically when the card moved to Done.

--- .next/dev/types/validator.ts
+++ .next/dev/types/validator.ts
@@ -36,10 +36,37 @@ type LayoutConfig<Route extends LayoutRoutes = LayoutRoutes> = {
 }
 
 
+// Validate ../../../app/about/page.tsx
+{
+  type __IsExpected<Specific extends AppPageConfig<"/about">> = Specific
+  const handler = {} as typeof import("../../../app/about/page.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
+// Validate ../../../app/features/page.tsx
+{
+  type __IsExpected<Specific extends AppPageConfig<"/features">> = Specific
+  const handler = {} as typeof import("../../../app/features/page.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../../app/page.tsx
 {
   type __IsExpected<Specific extends AppPageConfig<"/">> = Specific
   const handler = {} as typeof import("../../../app/page.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
+// Validate ../../../app/workflow/page.tsx
+{
+  type __IsExpected<Specific extends AppPageConfig<"/workflow">> = Specific
+  const handler = {} as typeof import("../../../app/workflow/page.js")
   type __Check = __IsExpected<typeof handler>
   // @ts-ignore
   type __Unused = __Check
